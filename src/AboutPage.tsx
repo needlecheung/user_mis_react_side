1c53bb758b08cc034bbc2870105598e2cc2fb626
needlecheung <770592173@qq.com>
2025-09-26 17:34:14 +0900

    完成了Step 7：React Router

---
+++ src/AboutPage.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>About</h2>
+      <p>This is a demo React app with routing.</p>
+    </div>
+  )
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
-import UsersPage from './UsersPage'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import UsersPage from "./UsersPage"
+import AboutPage from "./AboutPage"
+
 export default function App() {
-  return <UsersPage />
+  return (
+    <BrowserRouter>
+      <div style={{ padding: 20 }}>
+        {/* 导航菜单 */}
+        <nav style={{ marginBottom: 20 }}>
+          <Link to="/users" style={{ marginRight: 12 }}>Users</Link>
+          <Link to="/about">About</Link>
+        </nav>
+
+        {/* 路由配置 */}
+        <Routes>
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          {/* 默认跳转到 /users */}
+          <Route path="*" element={<UsersPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
